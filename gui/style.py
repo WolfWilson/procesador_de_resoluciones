5d5dfb31ede8c9ae915c04d854b394392dccb7dc
antobouvier <antonellabouvier@gmail.com>
2025-03-27 10:48:24 -0300

    modificacion en style.py

--- gui/style.py
+++ gui/style.py
@@ -1,35 +1,18 @@
-STYLE_MAIN = """
-QWidget {
-    background-color: #eefb09;
-    color: #f0f0f0;
-    font-family: Segoe UI, sans-serif;
-    font-size: 12px;
-}
 
+#modules/style.copy
+STYLE_MAIN = """
 QPushButton {
-    background-color: #3399ff;
-    color: #f0f0f0;
+    background-color: #eefb09;
+    color: #;
     padding: 8px;
-    border: none;
     border-radius: 6px;
 }
-QPushButton:hover {
-    background-color: #5aaeff;
-}
-QPushButton:pressed {
-    background-color: #2b89cc;
-}
-
 QProgressBar {
-    background-color: #2a2a2a;
-    border: 1px solid #444;
+    border: 1px solid #bbb;
     border-radius: 5px;
-    text-align: right;
-    color: #f0f0f0;
-    padding-right: 5px;
+    text-align: center;
 }
 QProgressBar::chunk {
-    background-color: #3399ff;
-    border-radius: 5px;
+    background-color: #eefb09;
 }
 """
